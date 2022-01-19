fa76a4ea7f005cc98b438f940b150d404a6edf9f
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-01-19 21:39:25 +0530

    Created get and post routes for postMessages

---
+++ server/controllers/posts.js
@@ -0,0 +1,24 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPost = async (req,res)=>{
+    try {
+        const postMessages = await PostMessage.find({}) 
+        // console.log(postMessages);
+        res.status(200).json(postMessages);
+    }
+    catch(err) {
+        res.status(404).json({message : error.message});
+    }
+    
+}
+
+export const createPost = async (req,res)=>{
+    const post = req.body;
+    try {
+        const newPost = new PostMessage(post);
+        const result = await newPost.save();
+        res.status(201).json(result);
+    }catch(error) {
+        res.status(409).json({message : error.message});
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from "express";
-
+import { createPost, getPost } from "../controllers/posts.js";
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.send('THIS WORKS');
-})
+router.get('/', getPost);
+router.post('/', createPost);
 
 export default router;
