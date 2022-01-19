8f74a57748d6b7e61fc5b046ba20882ba65b10e9
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-01-19 21:14:37 +0530

    Configured Mongo Db and setup Express.

--- server/index.js
+++ server/index.js
@@ -2,9 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import postRoutes from './routes/posts.js';
 const app = express();
 
 app.use(bodyParser.json({limit : "30mb", extended : true}));
 app.use(cors());
-app.use()
+app.use(bodyParser.urlencoded({limit :"30mb", extended : true}));
+
+
+//Routes Setup
+app.use('/posts', postRoutes);
+
+
+const CONNECTION_URL = 'mongodb+srv://kunals131:kunals131@cluster0.o0tf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//heroku will automatically populate env.PORT
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser : true, 
+    useUnifiedTopology : true,
+})
+.then(()=>app.listen(PORT, ()=>console.log(`SERVER RUNNING ON PORT http://localhost:${PORT}`)))
+.catch(ERR=>{
+    console.log(ERR.message);
+})
+
