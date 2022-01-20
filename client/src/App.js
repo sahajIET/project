e8129db5c4d8e332a7e4a3e46d9e1c528e30c031
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-01-20 11:22:28 +0530

    Added JSX Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-
-
+import './App.scss';
+import Form from "./components/Form/Form.jsx";
+import Posts from "./components/Posts/Posts.jsx";
 const App = ()=>{
    return (
-       <div>
-           <h1>Hello World!</h1>
+       <>
+       <div className="container">
+           <div className="form">
+               <Form/>
+           </div>
+           <div className="posts">
+               <Posts/>
+           </div>
        </div>
+       </>
    ) 
 }
+
+export default App;
---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Post from './Post/Post.jsx';
+const Posts = () => {
+  return <div>
+      <h1>POSTS</h1>
+      <Post/>
+      <Post/>
+  </div>;
+};
+
+export default Posts;
