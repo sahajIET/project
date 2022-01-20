3ee771697f9799b3f9cc1410582e0fe96612ae96
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-01-20 14:52:33 +0530

    Updated App.js with relevant styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from "react";
-import './App.scss';
-import {useEffect} from 'react';
-import {useDispatch} from 'react-redux'; 
-import {getPosts} from './actions/posts';
-import Form from "./components/Form/Form.jsx";
-import Posts from "./components/Posts/Posts.jsx";
-const App = ()=>{
-
-  
-   return (
-       <>
-
-       <div className="container">
-           <div className="form">
-               <Form/>
-           </div>
-           <div className="posts">
-               <Posts/>
-           </div>
-       </div>
-       </>
-   ) 
-}
+import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import Form from './components/Form/Form.jsx';
+import Posts from './components/Posts/Posts.jsx';
+import useStyles from './styles.js';
+const App = () => {
+    const classes = useStyles();
+  return (
+      <Container maxWidth="lg">
+          <AppBar className={classes.appBar} position='static' color='inherit'>
+              <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+              <img className={classes.image} src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI" alt="" height='60' />
+          </AppBar>
+          <Grow in>
+              <Container>
+                  <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                      <Grid item xs={12} sm={7}>
+                      <Posts></Posts>
+                      </Grid>
+                      <Grid item xs={12} sm={4}>
+                   
+                          <Form></Form>
+                      </Grid>
+                  </Grid>
+              </Container>
+          </Grow>
+      </Container>
+  )
+};
 
 export default App;
